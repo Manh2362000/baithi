8b9ac681870bee2d2484039d6dc09b5cc215dab5
Manh2362000 <70956659+Manh2362000@users.noreply.github.com>
2021-07-02 11:58:02 +0700

    gg

--- example-app/database/factories/ApartmentFactory.php
+++ example-app/database/factories/ApartmentFactory.php
@@ -7,18 +7,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ApartmentFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+
     protected $model = Apartment::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+
     public function definition()
     {
         return [
